757ec92db9e4fc8dbb6e40a77dc82b766b9bcbc9
Mary Marks <maryleemarks@gmail.com>
2021-08-12 18:38:49 -0700

    fix the api link

--- src/business.js
+++ src/business.js
@@ -1,6 +1,6 @@
 export default class exchangeRate {
   static getRate (currency){
-    return fetch('https://v6.exchangerate-api.com/v6/$process.env.API_KEY}/latest/${currency}')
+    return fetch('https://v6.exchangerate-api.com/v6/$process.env.API_KEY}/pair/USD/${currency}/${amount}')
     .then(function(response) {
       if (!response.ok) {
         throw Error(response.statusText);
