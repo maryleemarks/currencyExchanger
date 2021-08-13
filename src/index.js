09e32f377433ecd4a20cabde6f409d32491f2795
Mary Marks <maryleemarks@gmail.com>
2021-08-12 18:52:39 -0700

    fix code for errors and write getElements function

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import exchangeRate from './business';
 
-function showErrors(error) {
-  $('#showErrors').show;
-  $('#showErrors').text('Error: ${error}');
+function getElements(response) {
+  if (response.result === "success") {
+    $('.showConvertedCurrency').text('Your USD are equal to ${response.conversion_result} ${response.target_code}')
+  } else if (response["error-type"] === "unsupported-code") {
+    $('.showUnsupportedErrors').text("You did something weird and caused a super weird error. Please try another currency. That one was way too weird for us.");
+  } else {
+    $('.showErrors').text('Error: ${response.message}. Please try again.')
+  }  
+  }  
+
+  }
 }
 
 }
