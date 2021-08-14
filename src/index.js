d59773fbc7947f11ce49d0a3231b5921d961d679
Mary Marks <maryleemarks@gmail.com>
2021-08-13 20:23:46 -0700

    fix a few more typos. Break for the day

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ $(document).ready(function() {
   $('#convert').click(function() {
     let base_code = $('#base_code').val();
     let target_code = $('#target_code').val();
-  //  const parseIntConversion=parseFloat(${body.conversion_rate});
-  //  const convertedCurrency=(${parseIntConversion}*${base_code});
-    let promise = ExchangeRate.getCurrency(target_code, base_code);
+    let promise = ExchangeRate.getRate(target_code, base_code);
+    //const parseIntConversion=parseFloat(${body.conversion_rate});
+    //const convertedCurrency=(${parseIntConversion}*${base_code});
 
     promise.then(function(response) {
       const body = JSON.parse(response); 
       $('.showConvertedCurrency').text(`Your USD are equal to ${body.conversion_rate}`);
     }, function(error) {
-        $('#.showErrors').text(`Error: ${error}`);  
+      $('#.showErrors').text(`Error: ${error}`);  
     });
   });
 });
